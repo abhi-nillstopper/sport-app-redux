195399051184bc18c2a767252fcda98a3440876b
Abhi Shah <abhi41348@gmail.com>
2021-06-04 14:36:21 +0530

    redux dev tools

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -13,9 +13,9 @@ const store = createStore(
   InitialState,
   compose(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__
-      ? window.__REDUX_DEVTOOLS_EXTENSION__()
-      : (f) => f
+    typeof window.__REDUX_DEVTOOLS_EXTENSION__ === "undefined"
+      ? (a) => a
+      : window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
 export default store;
