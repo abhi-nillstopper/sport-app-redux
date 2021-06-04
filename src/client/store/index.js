4b6460bdc061e42d0470cf44507be7c627615793
Abhi Shah <abhi41348@gmail.com>
2021-06-04 14:28:24 +0530

    redux dev tools

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -6,16 +6,16 @@ const InitialState = {};
 
 const middleware = [thunk];
 
-console.log("NODE_ENV",process.env.NODE_ENV)
+console.log("NODE_ENV", process.env.NODE_ENV);
 
 const store = createStore(
   All_Reducer,
   InitialState,
   compose(
     applyMiddleware(...middleware),
-    process.env.NODE_ENV === "development" &&
-      window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__()
+    window.__REDUX_DEVTOOLS_EXTENSION__
+      ? window.__REDUX_DEVTOOLS_EXTENSION__()
+      : (f) => f
   )
 );
 export default store;
